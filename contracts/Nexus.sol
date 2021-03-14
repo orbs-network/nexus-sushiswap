ae114e12611960da948a37ffa6f8209954b88bcc
DanielZlotin <zlotindaniel@gmail.com>
2021-03-14 21:17:29 +0200

    playing around

--- contracts/Nexus.sol
+++ contracts/Nexus.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.7.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
-contract Nexus is Ownable {
+contract Nexus is Ownable, ERC20("NexusEthUSDC", "NexusEthUSDC") {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
@@ -45,17 +46,41 @@ contract Nexus is Ownable {
 
     // --- sponsor actions ---
 
-    function deposit(uint256 amount) onlySponsor {
-        // TODO
-    }
-
-    function withdraw(uint256 amount) onlySponsor {
-        // TODO is this needed? exiting without emergency?
-    }
+    //    function deposit(uint256 amount) onlySponsor {
+    //        // TODO
+    //    }
+    //
+    //    function withdraw(uint256 amount) onlySponsor {
+    //        // TODO is this needed? exiting without emergency?
+    //    }
 
     function emergencyWithdraw() external onlySponsor {
         // TODO exit position to free USDC?
         uint256 balance = IERC20(USDC).balanceOf(address(this));
         IERC20(USDC).safeTransfer(sponsor, balance);
+    }
+
+    // ---------------------------------- for testing the basic logic: ----------------------------------
+    // assuming infinite USDC
+
+    uint256 priceInUSD = 0;
+
+    function setEthPrice(uint256 _priceInUSD) public {
+        priceInUSD = _priceInUSD;
+    }
+
+    function deposit(uint256 ethAmount) public onlyOwner {
+        require(msg.value >= ethAmount, "insufficient value for deposit");
+        uint256 sharesBefore = totalSupply();
+        uint256 shares = ethAmount.mul(totalSupply()).div(sharesBefore);
+        _mint(owner(), shares);
+    }
+
+    function withdraw(uint256 shares) public onlyOwner {
+        _burn(owner(), shares);
+    }
+
+    function withdrawAll() public onlyOwner {
+        withdraw(balanceOf(owner()));
     }
 }
--- test/nexus-test.ts
+++ test/nexus-test.ts
@@ -3,14 +3,35 @@ import { Wallet } from "../src/impl/wallet";
 import { Nexus } from "../typechain-hardhat/Nexus";
 import { expect } from "chai";
 import { Tokens } from "../src/impl/token";
+import { bn18, zero } from "../src/utils";
 
 describe("Nexus", () => {
   it.only("sponsor can emergencyWithdraw", async () => {
     const deployer = await Wallet.fake();
-    const nexus = await localContract("Nexus", deployer.address, [deployer.address]);
+    const nexus = await localContract<Nexus>("Nexus", deployer.address, [deployer.address]);
     expect(await nexus.methods.USDC().call()).eq(Tokens.eth.USDC().address);
   });
 
-  function deposit(amount: number) {}
-  function withdraw(amount: number) {}
+  it.only("deposit & withdraw with shares", async () => {
+    const deployer = await Wallet.fake();
+    const owner = deployer.address;
+    const nexus = await localContract<Nexus>("Nexus", owner, [owner]);
+    expect(await nexus.methods.totalSupply().call()).bignumber.eq(zero);
+
+    await nexus.methods.deposit(bn18("100")).send({ from: owner });
+    expect(await nexus.methods.balanceOf(owner).call()).bignumber.eq(bn18("100"));
+    expect(await nexus.methods.totalSupply().call()).bignumber.eq(bn18("100"));
+
+    await nexus.methods.deposit(bn18("100")).send({ from: owner });
+    expect(await nexus.methods.balanceOf(owner).call()).bignumber.eq(bn18("200"));
+    expect(await nexus.methods.totalSupply().call()).bignumber.eq(bn18("200"));
+
+    await nexus.methods.withdraw(bn18("100")).send({ from: owner });
+    expect(await nexus.methods.balanceOf(owner).call()).bignumber.eq(bn18("100"));
+    expect(await nexus.methods.totalSupply().call()).bignumber.eq(bn18("100"));
+
+    await nexus.methods.withdrawAll().send({ from: owner });
+    expect(await nexus.methods.balanceOf(owner).call()).bignumber.eq(zero);
+    expect(await nexus.methods.totalSupply().call()).bignumber.eq(zero);
+  });
 });
