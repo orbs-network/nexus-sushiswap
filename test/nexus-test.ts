23978826ee42ed08b847255c10eef2bc820c4b1d
DanielZlotin <zlotindaniel@gmail.com>
2021-03-14 19:48:59 +0200

    fix test

--- test/nexus-test.ts
+++ test/nexus-test.ts
@@ -1,5 +1,4 @@
-import { contract, Contract } from "../src/extensions";
-import { artifact, network, web3 } from "../src/network";
+import { localContract } from "../src/extensions";
 import { Wallet } from "../src/impl/wallet";
 import { Nexus } from "../typechain-hardhat/Nexus";
 import { expect } from "chai";
@@ -8,7 +7,10 @@ import { Tokens } from "../src/impl/token";
 describe("Nexus", () => {
   it.only("sponsor can emergencyWithdraw", async () => {
     const deployer = await Wallet.fake();
-
+    const nexus = await localContract("Nexus", deployer.address, [deployer.address]);
     expect(await nexus.methods.USDC().call()).eq(Tokens.eth.USDC().address);
   });
+
+  function deposit(amount: number) {}
+  function withdraw(amount: number) {}
 });
