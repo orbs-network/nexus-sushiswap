15c25ce04e8eadabf7a8ca604679640ebe32b9bb
DanielZlotin <zlotindaniel@gmail.com>
2021-03-21 13:16:04 +0200

    configFile

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,23 @@
 import fs from "fs";
 import path from "path";
 
-export interface ConfigFile {
-  coinmarketcapKey: string;
-  infuraKey: string;
-  alchemyKey: string;
-  etherscanKey: string;
-}
-export function config(): ConfigFile {
+export function config() {
   const p = process.env.npm_package_config_file || "";
-  if (!fs.existsSync(p)) throw new Error(`must provide a config file in package.json`);
-  return JSON.parse(fs.readFileSync(path.resolve(p), { encoding: "utf8" })) as ConfigFile;
+  if (!fs.existsSync(p)) {
+    fs.writeFileSync(
+      p,
+      JSON.stringify(
+        {
+          coinmarketcapKey: "APIKEY",
+          infuraKey: "APIKEY",
+          alchemyKey: "APIKEY",
+          etherscanKey: "APIKEY",
+        },
+        null,
+        4
+      ),
+      { encoding: "utf8" }
+    );
+  }
+  return JSON.parse(fs.readFileSync(path.resolve(p), { encoding: "utf8" }));
 }
